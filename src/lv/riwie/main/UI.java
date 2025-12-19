4741f1834096fe3645d40b9d2645f45e2fb228dc
Ēriks Boka <e@boka.lv>
2025-12-19 13:24:20 +0200

    UI cleaned

--- src/lv/riwie/main/UI.java
+++ src/lv/riwie/main/UI.java
@@ -3,16 +3,13 @@ package lv.riwie.main;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics2D;
-import java.awt.image.BufferedImage;
 import java.text.DecimalFormat;
-
-import lv.riwie.object.OBJ_Key;
 
 public class UI {
     GamePanel gp;
+    Graphics2D g2;
     Font gameFont = new Font("Minecraft RUS", Font.PLAIN, 40);
     Font congratulationsFont = new Font("Minecraft RUS", Font.BOLD, 50);
-    BufferedImage keyImage;
     public boolean messageOn = false;
     public String message = "";
     int messageCounter = 0;
@@ -22,8 +19,7 @@ public class UI {
 
     public UI(GamePanel gp) {
         this.gp = gp;
-        OBJ_Key key = new OBJ_Key(gp);
-        keyImage = key.image;
+
     }
 
     public void showMessage(String text) {
@@ -32,60 +28,26 @@ public class UI {
     }
 
     public void draw(Graphics2D g2) {
-
-        if (gameFinished == true) {
-
-            g2.setFont(gameFont);
-            g2.setColor(Color.white);
-
-            String text;
-            int textLength;
-            int x;
-            int y;
-
-            text = "You found the treasure!";
-            textLength = (int) g2.getFontMetrics().getStringBounds(text, g2).getWidth();
-            x = gp.screenWidth / 2 - textLength / 2;
-            y = gp.screenHeight / 2 - (gp.tileSize * 3);
-            g2.drawString(text, x, y);
-
-            text = "Your time is: " + dFormat.format(playTime) + "!";
-            textLength = (int) g2.getFontMetrics().getStringBounds(text, g2).getWidth();
-            x = gp.screenWidth / 2 - textLength / 2;
-            y = gp.screenHeight / 2 - (gp.tileSize * 5);
-            g2.drawString(text, x, y);
-
-            g2.setFont(congratulationsFont);
-            g2.setColor(Color.yellow);
-            text = "Congratulations!";
-            textLength = (int) g2.getFontMetrics().getStringBounds(text, g2).getWidth();
-            x = gp.screenWidth / 2 - textLength / 2;
-            y = gp.screenHeight / 2 + (gp.tileSize * 2);
-            g2.drawString(text, x, y);
-
-            gp.gameThread = null;
-
-        } else {
-
-            g2.setFont(gameFont);
-            g2.setColor(Color.white);
-            g2.drawImage(keyImage, gp.tileSize / 2, gp.tileSize / 2, gp.tileSize, gp.tileSize, null);
-            g2.drawString("x" + gp.player.hasKey, 75, 50);
-
-            playTime += (double) 1 / 60;
-            g2.drawString("Time" + dFormat.format(playTime), gp.tileSize * 11, 50);
-
-            if (messageOn == true) {
-                // gp.setFont(gp.getFont(gameFont).deriveFont(30F));
-                g2.drawString(message, gp.tileSize * 4, gp.tileSize * 10);
-                messageCounter++;
-                if (messageCounter > 120) {
-
-                    messageCounter = 0;
-                    messageOn = false;
-                }
-            }
+        this.g2 = g2;
+        g2.setFont(gameFont);
+        g2.setColor(Color.white);
+        if (gp.gameState == gp.playState) {
+            // playstate
+        } else if (gp.gameState == gp.pauseState) {
+            drawPauseScreen();
         }
+    }
 
+    public void drawPauseScreen() {
+        String text = "Paused";
+        int x = getXforCenteredText(text);
+        int y = gp.screenHeight/2;
+
+        g2.drawString(text, x, y);
+    }
+    public int getXforCenteredText(String text) {
+        int len = (int)g2.getFontMetrics().getStringBounds(text, g2).getWidth();
+        int x = gp.screenWidth/2 - len/2;
+        return x;
     }
 }
